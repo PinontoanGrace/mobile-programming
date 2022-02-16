b61ee79aa0bb11999700538cfcfc0123e2b098c0
=Matthew-Muju <s21810058@student.unklab.ac.id>
2022-02-16 14:49:46 +0800

    Menambahkan Fakultas

--- Exercise#1/name.js
+++ Exercise#1/name.js
@@ -3,3 +3,5 @@ console.log("Muju Matthew Edgar Freeman");
 console.log("Class : Mobile Development Application");
 
 console.log("Prodi : Informatika");
+
+console.log("Faculty : Computer Science");
